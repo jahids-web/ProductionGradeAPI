3075577654587e9b10c4ed6415246932149ed4a2
Jahid Uddin <jahiduddin922@gmail.com>
2022-02-19 11:15:37 +0600

    StudentApi-Project/DLL

---
+++ StudentApi-Project/DLL/DLLDependency.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DLL
+{
+    public static class DLLDependency
+    {
+        public static void AllDepandency(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+              options.UseSqlServer(configuration.GetConnectionString("StudentDatabase")));
+
+        }
+    }
+}
