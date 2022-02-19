1ddec60c20a5650eb1ad695b8bd7b50c59fc566d
Jahid Uddin <jahiduddin922@gmail.com>
2022-02-19 10:29:00 +0600

    Database Setap

--- StudentApi-Project/StudentApi/Program.cs
+++ StudentApi-Project/StudentApi/Program.cs
@@ -1,3 +1,7 @@
+using DLL;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+//ConfigureServices
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("StudentDatabase")));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
