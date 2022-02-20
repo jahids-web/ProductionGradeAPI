560b18ac72887680d92b3e18f5277237677d2d19
Jahid Uddin <jahiduddin922@gmail.com>
2022-02-20 12:20:06 +0600

    API-Project

--- API-Project/API/Controllers/DepartmentController.cs
+++ API-Project/API/Controllers/DepartmentController.cs
@@ -3,8 +3,39 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class DepartmentController : ControllerBase
     {
-       
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Get all Student");
+        }
+
+        [HttpGet("{code}")]
+        public IActionResult GetA(string code)
+        {
+            return Ok("get "+ code + "department data");
+        }
+
+        [HttpPost]
+        public IActionResult Insert(string code)
+        {
+            return Ok("Insert New Department");
+        }
+
+        [HttpPut("{code}")]
+        public IActionResult Updat(string code)
+        {
+            return Ok("Update " + code + "deparmet data");
+        }
+
+        [HttpGet("{code}")]
+        public IActionResult Delete(string code)
+        {
+            return Ok("Delete " + code + "deparmet data");
+        }
+
     }
 }
