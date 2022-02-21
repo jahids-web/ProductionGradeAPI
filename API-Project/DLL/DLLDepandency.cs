3010f722bfc64d4155e1a32b39562e245f35b3aa
Jahid Uddin <jahiduddin922@gmail.com>
2022-02-21 12:28:22 +0600

    DLL Project

---
+++ API-Project/DLL/DLLDepandency.cs
@@ -0,0 +1,19 @@
+﻿using DLL.DataContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLL
+{
+    public class DLLDepandency
+    {
+        public static void AllDepandency(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DatabaseConnection")));
+        }
+    }
+}
---
+++ API-Project/DLL/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace DLL.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
