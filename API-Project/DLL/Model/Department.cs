7b06e84bdf7be7b7e8a08b1f5178c61370cae6b6
Jahid Uddin <jahiduddin922@gmail.com>
2022-02-21 12:45:16 +0600

    Add Dll Model

--- API-Project/DLL/Model/Department.cs
+++ API-Project/DLL/Model/Department.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace API.Models
+namespace DLL.Models
 {
     public class Department
     {
+        public int DepartmentId { get; set; }
         public string Name { get; set; }
         public string  Code { get; set; }
 
--- API-Project/DLL/Model/Student.cs
+++ API-Project/DLL/Model/Student.cs
@@ -1,7 +1,8 @@
-﻿namespace API.Models
+﻿namespace DLL.Models
 {
     public class Student
     {
+        public int StudentId { get; set; }
         public string Name{ get; set; }
         public string Email{ get; set; }
     }
