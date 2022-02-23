361ee251e1095b4bcaf1847b357e5a7626b4fdd2
Jahid Uddin <jahiduddin922@gmail.com>
2022-02-23 21:35:34 +0600

    Add New Student Services/IDepartmentService File

--- API-Project/BLL/Services/IDepartmentService.cs
+++ API-Project/BLL/Services/IDepartmentService.cs
@@ -1,16 +1,50 @@
-﻿using System;
+﻿using DLL.Models;
+using DLL.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BLL.Services
 {
     public interface IDepartmentService
     {
-
+        Task<Department> InsertAsync(Department department);
+        Task<List<Department>> GetAllAsync();
+        Task<Department> UpdateAsync(string code, Department department);
+        Task<Department> DeleteAsync(string code);
+        Task<Department> GetAAsync(string code);
     }
 
-    public class DepartmentService : IDepartmentService;
+    public class DepartmentService : IDepartmentService
     {
+        private IDepartmentRepository _departmentRepository;
 
+        public DepartmentService(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        public async Task<Department> InsertAsync(Department department)
+        {
+            return await _departmentRepository.InsertAsync(department); 
+        }
+        public async Task<List<Department>> GetAllAsync()
+        {
+            return await _departmentRepository.GetAllAsync();
+        }
+        public async Task<Department> GetAAsync(string code)
+        {
+            return await _departmentRepository.GetAAsync(code);
+        }
+      
+        public async Task<Department> UpdateAsync(string code, Department department)
+        {
+            return await _departmentRepository.UpdateAsync(code, department);
+        }
+        public async Task<Department> DeleteAsync(string code)
+        {
+            return await _departmentRepository.DeleteAsync(code);
+        }
     }
 }
---
+++ API-Project/BLL/Services/IStudentRepository.cs
@@ -0,0 +1,48 @@
+﻿using DLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public interface IStudentRepository
+    {
+        Task<Student> InsertAsync(Student student);
+        Task<List<Student>> GetAllAsync();
+        Task<Student> UpdateAsync(string email, Student student);
+        Task<Student> DeleteAsync(string email);
+        Task<Student> GetAAsync(string email);
+    }
+
+    public class StudentRepository : IStudentRepository
+    {
+        private IStudentRepository _studnetRepository;
+
+        public StudentRepository(IStudentRepository studentRepository)
+        {
+            _studnetRepository = studentRepository;
+        }
+
+        public async Task<Student> InsertAsync(Student student)
+        {
+            return await _studnetRepository.InsertAsync(student);
+        }
+        public async Task<List<Student>> GetAllAsync()
+        {
+            return await _studnetRepository.GetAllAsync();
+        }
+        public async Task<Student> GetAAsync(string email)
+        {
+            return await _studnetRepository.GetAAsync(email);
+        }
+        public async Task<Student> UpdateAsync(string email, Student student)
+        {
+            return await _studnetRepository.UpdateAsync(email,student);
+        }
+        public async Task<Student> DeleteAsync(string email)
+        {
+            return await _studnetRepository.DeleteAsync(email);
+        }
+    }
+}
