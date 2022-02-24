ba729ed3c7aa760d2c2466f3aa0e6eb888971de7
Jahid Uddin <jahiduddin922@gmail.com>
2022-02-24 23:28:25 +0600

    Custom validation

--- API-Project/BLL/Request/DepartmentInsertRequestViewModel.cs
+++ API-Project/BLL/Request/DepartmentInsertRequestViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,19 @@ namespace BLL.Request
 {
     public class DepartmentInsertRequestViewModel
     {
+        public string Name { get; set; }    
+        public string Code { get; set; } 
+    }
+
+    public class DepartmentInsertRequestViewModelValidator : AbstractValidator<DepartmentInsertRequestViewModel>
+    {
+        public DepartmentInsertRequestViewModelValidator()
+        {
+            RuleFor(x => x.Name).NotNull().NotEmpty().MaximumLength(4).MinimumLength(25);
+            RuleFor(x => x.Code).NotNull().NotEmpty().MinimumLength(3).MaximumLength(10);
+            
+        }
+
+       
     }
 }
