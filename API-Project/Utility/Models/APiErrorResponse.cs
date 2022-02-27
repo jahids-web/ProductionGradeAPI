f2397fa4df0e550a2cdfbc5a8484f6c977f4e178
Jahid Uddin <jahiduddin922@gmail.com>
2022-02-27 20:35:02 +0600

    Edit Models APiErrorResponse.cs

--- API-Project/Utility/Models/APiErrorResponse.cs
+++ API-Project/Utility/Models/APiErrorResponse.cs
@@ -8,6 +8,7 @@ namespace Utility.Models
     {
         public string Message { get; set; } 
         public string Details { get; set; }
-        public bool IsSuccess { get; set; }
+        public bool IsSuccess { get; set; } = false;
+        public int StatusCode { get; set; }
     }
 }
