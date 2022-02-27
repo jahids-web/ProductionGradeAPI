5c0850e7cb76efbbb69fa4b33483cf28f0fe62ab
Jahid Uddin <jahiduddin922@gmail.com>
2022-02-27 19:51:07 +0600

    Add new Models/APiErrorResponse.cs

---
+++ API-Project/Utility/Models/APiErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Models
+{
+    public class APiErrorResponse
+    {
+        public string Message { get; set; } 
+        public string Details { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
