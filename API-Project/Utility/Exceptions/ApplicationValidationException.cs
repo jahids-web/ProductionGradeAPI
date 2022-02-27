6949b5baa8bea3283202970f8ab86119fa94bcce
Jahid Uddin <jahiduddin922@gmail.com>
2022-02-27 11:48:43 +0600

    Add new Utility project

---
+++ API-Project/Utility/Exceptions/ApplicationValidationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility.Exceptions
+{
+    public class ApplicationValidationException : Exception
+    {
+        public ApplicationValidationException(string message):base(message)
+        {
+
+        }
+
+    }
+}
