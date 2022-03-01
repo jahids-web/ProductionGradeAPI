1264bec54c599a966a7d4ff38800c97d44d7b3e0
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-01 17:59:34 +0600

    DLL Add Migrations

--- API-Project/DLL/DataContext/ApplicationDbContext.cs
+++ API-Project/DLL/DataContext/ApplicationDbContext.cs
@@ -2,21 +2,26 @@
 using DLL.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DLL.DataContext
 {
     public class ApplicationDbContext : DbContext
     {
         private const string _isDeletedProperty = "IsDeleted";
-        private static readonly MethodInfo _propertyMethod = typeof(EF).GetMethod(nameof(EF.Property), BindingFlags.Static | BindingFlags.Public).MakeGenericMethod(typeof(bool));
-        private string IsDeletedProperty;
+
+        private static readonly MethodInfo _propertyMethod = typeof(EF)
+            .GetMethod(nameof(EF.Property), BindingFlags.Static | BindingFlags.Public).MakeGenericMethod(typeof(bool));
+
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-
         }
+
         private static LambdaExpression GetIsDeletedRestriction(Type type)
         {
             var parm = Expression.Parameter(type, "it");
@@ -25,19 +30,48 @@ namespace DLL.DataContext
             var lambda = Expression.Lambda(condition, parm);
             return lambda;
         }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             foreach (var entity in modelBuilder.Model.GetEntityTypes())
             {
-                if(typeof(ISoftDeletTable).IsAssignableFrom(entity.ClrType)== true)
+                if (typeof(ISoftDeletTable).IsAssignableFrom(entity.ClrType) == true)
                 {
-                    entity.AddProperty(IsDeletedProperty, typeof(bool));
+                    // entity.AddProperty(IsDeletedProperty, typeof(bool));
+                    // modelBuilder.Entity(entity.ClrType).HasQueryFilter(GetIsDeletedRestriction(entity.ClrType));
+
+                    entity.AddProperty(_isDeletedProperty, typeof(bool));
                     modelBuilder.Entity(entity.ClrType).HasQueryFilter(GetIsDeletedRestriction(entity.ClrType));
                 }
             }
-           
+
             base.OnModelCreating(modelBuilder);
         }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            await OnBeforeSaving();
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        private async Task OnBeforeSaving()
+        {
+            ChangeTracker.DetectChanges();
+            var entries = ChangeTracker.Entries()
+                .Where(e => e.State != EntityState.Detached && e.State != EntityState.Unchanged);
+
+            foreach (var entry in entries)
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Deleted:
+                        entry.Property(_isDeletedProperty).CurrentValue = true;
+                        entry.State = EntityState.Modified;
+                        break;
+                }
+            }
+        }
+
 
         public DbSet<Department> Departments { get; set; }
         public DbSet<Student> students { get; set; }
