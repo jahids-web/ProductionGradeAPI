0bad84b876f337ea61e1ad9e924527855c80df21
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-05 16:09:15 +0600

    Edit DLLDepandency.cs

--- API-Project/DLL/DLLDepandency.cs
+++ API-Project/DLL/DLLDepandency.cs
@@ -1,11 +1,8 @@
 ﻿using DLL.DataContext;
 using DLL.Repositories;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
+
 
 namespace DLL
 {
@@ -13,13 +10,14 @@ namespace DLL
     {
         public static void AllDepandency(IServiceCollection services, IConfiguration configuration)
         {
-            var mm = configuration.GetConnectionString("DatabaseConnection");
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DatabaseConnection")));
+            //var mm = configuration.GetConnectionString("DatabaseConnection");
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(configuration.GetConnectionString("DatabaseConnection")));
 
             //repository dependency
-            services.AddTransient<IDepartmentRepository,DepartmentRepository>();
-            services.AddTransient<IStudentRepository,StudentRepository>();
+            //services.AddTransient<IUnitOfWork, UnitOfWork.UnitOfWork>();
+            services.AddTransient<IDepartmentRepository, DepartmentRepository>();
+            services.AddTransient<IStudentRepository, StudentRepository>();
         }
     }
 }
