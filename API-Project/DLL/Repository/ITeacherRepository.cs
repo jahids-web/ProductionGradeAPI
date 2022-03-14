37d13911a93c71ae4978f3bc558f4ea7ef33beb1
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-14 16:13:10 +0600

    Add ITearcherRepository file

---
+++ API-Project/DLL/Repository/ITeacherRepository.cs
@@ -0,0 +1,18 @@
+﻿using DLL.DbContext;
+using DLL.Model;
+using DLL.UniteOfWork;
+
+namespace DLL.Repository
+{
+    public interface ITeacherRepository:IRepositoryBase<Teacher>
+    {
+
+    }
+
+    public class TeacherRepository : RepositoryBase<Teacher>, ITeacherRepository
+    {
+        public TeacherRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
