3c3ff840b4f58dd215d70c680fe5f160ff4069af
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-16 21:16:48 +0600

    Add New Model CourseStudent

---
+++ API-Project/DLL/Model/CourseStudent.cs
@@ -0,0 +1,16 @@
+﻿using DLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLL.Model
+{
+    public class CourseStudent
+    {
+        public int CourseId { get; set; }
+        public Course Course { get; set; }  
+        public int StudentId { get; set; }  
+        public Student Student { get; set; }
+
+    }
+}
