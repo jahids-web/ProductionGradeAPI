f767809b77b0b8bb776ab2f756aa3003f54603f1
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-17 20:03:35 +0600

    Add New Depandency

--- API-Project/DLL/DLLDepandency.cs
+++ API-Project/DLL/DLLDepandency.cs
@@ -16,6 +16,7 @@ namespace DLL
 
             services.AddTransient<IStudentRepository, StudentRepository>();
             services.AddTransient<IDepartmentRepository, DepartmentRepository>();
+            services.AddTransient<ICourseRepository, CourseRepository>();
            
         }
     }
---
+++ API-Project/DLL/Repository/ICourseRepository.cs
@@ -0,0 +1,19 @@
+﻿using DLL.DbContext;
+using DLL.Model;
+using DLL.Models;
+
+namespace DLL.UniteOfWork
+{
+    public interface ICourseRepository : IRepositoryBase<Course>
+    {
+     
+    }
+
+    public class CourseRepository : RepositoryBase<Course>, ICourseRepository
+    {
+        public CourseRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+    }
+}
