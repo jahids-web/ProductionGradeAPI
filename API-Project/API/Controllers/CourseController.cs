57870dd5c0d000373ef3bc574365c506a32f07c2
Jahid Uddin <jahiduddin922@gmail.com>
2022-03-17 20:01:22 +0600

    Add New Controllers/CourseController

---
+++ API-Project/API/Controllers/CourseController.cs
@@ -0,0 +1,54 @@
+﻿using BLL.Services;
+using DLL.Model;
+using DLL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok( await _courseService.GetAllAsync());
+        }
+
+        [HttpGet("{code}")]
+        public async Task<IActionResult> GetA(string code)
+        {
+            return Ok(await _courseService.GetAAsync(code));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(Course course)
+        {
+            return Ok(await _courseService.InsertAsync(course));
+        }
+
+        [HttpPut("{code}")]
+        public async Task<IActionResult> Update(string code, Course course)
+        {
+            return Ok(await _courseService.UpdateAsync(code, course));
+        }
+
+        [HttpDelete("{code}")]
+        public async Task<IActionResult> Delete(string code)
+        {
+            return Ok(await _courseService.DeleteAsync(code));
+        }
+
+    }
+
+  
+}
